23c21469124d3452ec8f1d1fde1a1ea9c4107e31
xxxxxyn <343759189@qq.com>
2019-12-19 19:55:56 +0800

    1219

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <tree :treeData="treeData"></tree>
+  </div>
+</template>
+
+<script>
+  import tree from './components/tree'
+
+  let treeData = [
+    {
+      text: "一级-1",
+      children: [
+        {
+          text: "二级-1",
+        },
+        {
+          text: "二级-2",
+          children: [
+            {
+              text: "三级-1",
+            },
+            {
+              text: "三级-2",
+            }
+          ]
+        }
+      ]
+    },
+    {
+      text: "一级-2",
+    }
+  ];
+
+  export default {
+    name: 'app',
+    components: {
+      tree
+    },
+    data(){
+      return {
+        treeData
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/tree.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul>
+    <li v-for="item in treeData" :key='item.text'>
+      <input type="button" v-if="item.children" @click="openList()" v-model="openText">
+      {{item.text}}
+      <tree :treeData="item.children" v-if="isShow"></tree>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+
+
+  export default {
+    name: "tree",
+    props: {
+      treeData: {
+        default: function () {
+          return []
+        }
+      },
+
+    },
+    data() {
+      return {
+        isShow: false,
+        openText:'展开'
+      }
+    },
+    methods: {
+      openList() {
+        this.isShow = !this.isShow
+        if(this.isShow!==false){
+          this.openText='关闭'
+        }else{
+          this.openText='展开'
+        }
+
+      }
+
+    }
+  }
+</script>
+
+<style scoped>
+*{
+  list-style: none;
+}
+</style>
